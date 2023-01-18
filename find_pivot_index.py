52e2b6cb610a5ab837f8b4201b7b7e593eabc1f7
Muhammad Sameer <muhammadsameer.css@gmail.com>
2023-01-19 02:33:07 +0500

    problem 2

---
+++ find_pivot_index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        total_sum = sum(nums)
+        for i, num in enumerate(nums):
+            if left_sum == total_sum - left_sum - num:
+                return i
+            left_sum += num
+
+        return -1
