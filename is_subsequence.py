139ab68dd66bf8f4c0095a35bbefdb0ededc4ec4
Muhammad Sameer <muhammadsameer.css@gmail.com>
2023-01-19 03:14:32 +0500

    problem 4

---
+++ is_subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                print("i:", i)
+            j += 1
+            print(j)
+        return i == len(s)
